b92dacf7fe0cda8973ca1b699b6ccf13cfb0e197
matthew-gordon <m.gordon5525@gmail.com>
2024-12-30 22:01:37 -0700

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      include: ['lib'],
+    }),
+  ],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime', 'graphql'],
+    },
+  },
+});
